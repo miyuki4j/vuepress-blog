c5612e378141525bd9fdda951f74f8193ee8ec2f
miyuki4j <rebirthypp@163.com>
2024-01-15 18:40:05 +0800

    add daily records.

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,51 +2,63 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/demo/",
   {
-    text: "博文",
+    text: "每日一记",
     icon: "pen-to-square",
-    prefix: "/posts/",
+    prefix: "/daily/",
     children: [
       {
-        text: "苹果",
+        text: "2024-01-15",    
         icon: "pen-to-square",
-        prefix: "apple/",
-        children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
-        ],
+        link: "2024-01-15"
       },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
-    ],
+    ]
   },
+  "/demo/",
   {
     text: "V2 文档",
     icon: "book",
     link: "https://theme-hope.vuejs.press/zh/",
   },
+  // {
+  //   text: "水果",
+  //   icon: "pen-to-square",
+  //   prefix: "/posts/",
+  //   children: [
+  //     {
+  //       text: "苹果",
+  //       icon: "pen-to-square",
+  //       prefix: "apple/",
+  //       children: [
+  //         { text: "苹果1", icon: "pen-to-square", link: "1" },
+  //         { text: "苹果2", icon: "pen-to-square", link: "2" },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     {
+  //       text: "香蕉",
+  //       icon: "pen-to-square",
+  //       prefix: "banana/",
+  //       children: [
+  //         {
+  //           text: "香蕉 1",
+  //           icon: "pen-to-square",
+  //           link: "1",
+  //         },
+  //         {
+  //           text: "香蕉 2",
+  //           icon: "pen-to-square",
+  //           link: "2",
+  //         },
+  //         "3",
+  //         "4",
+  //       ],
+  //     },
+  //     { text: "樱桃", icon: "pen-to-square", link: "cherry" },
+  //     { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
+  //     "tomato",
+  //     "strawberry",
+  //   ],
+  // },
 ]);
