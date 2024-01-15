6d8a022a17c5fdbac8f992a81ad5b85abd983474
miyuki4j <39372705+miyuki4j@users.noreply.github.com>
2024-01-15 10:44:36 +0000

    Deploying to gh-pages from @ miyuki4j/vuepress-blog@a563c1e5447f8f4e5077969368e9b3776360252b 🚀

---
+++ assets/2024-01-15.html-84bfd55b.js
@@ -0,0 +1,14 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as a,f as d}from"./app-650a71b8.js";const s={},n=d(`<h1 id="github-源码" tabindex="-1"><a class="header-anchor" href="#github-源码" aria-hidden="true">#</a> github 源码</h1><h2 id="_1-oceanbase-官方教程" tabindex="-1"><a class="header-anchor" href="#_1-oceanbase-官方教程" aria-hidden="true">#</a> 1.oceanbase 官方教程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>https://oceanbase.github.io/miniob/miniob-introduction.html
+https://github.com/oceanbase/miniob
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-国产可持久化防redis-数据库" tabindex="-1"><a class="header-anchor" href="#_2-国产可持久化防redis-数据库" aria-hidden="true">#</a> 2.国产可持久化防redis 数据库</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>pika: https://github.com/OpenAtomFoundation/pika
+ssdb: https://github.com/ideawu/ssdb
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_3-cmu-15445-课程" tabindex="-1"><a class="header-anchor" href="#_3-cmu-15445-课程" aria-hidden="true">#</a> 3.cmu 15445 课程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>https://github.com/cmu-db/bustub
+
+官方课程:
+https://15445.courses.cs.cmu.edu/spring2024/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_4-rust-学习资料" tabindex="-1"><a class="header-anchor" href="#_4-rust-学习资料" aria-hidden="true">#</a> 4.rust 学习资料</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>1.课程练习: https://github.com/sunface/rust-by-practice
+2.翻译文档: 
+    https://course.rs/about-book.html
+    https://kaisery.github.io/trpl-zh-cn/
+    https://blog.fudenglong.site/Programming-Rust/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,9),c=[n];function r(t,o){return i(),a("div",null,c)}const h=e(s,[["render",r],["__file","2024-01-15.html.vue"]]);export{h as default};
