3fead8bfe2640aa97c74d234f51bea8143d3b0dc
miyuki4j <39372705+miyuki4j@users.noreply.github.com>
2024-01-17 11:14:10 +0000

    Deploying to gh-pages from @ miyuki4j/vuepress-blog@9e67c7fd71c5e6ecfbe752c8d8e5cfaa13406f79 🚀

---
+++ assets/2024-01-15.html-7d19d447.js
@@ -0,0 +1,22 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as i,f as d}from"./app-3e1b09c5.js";const s={},n=d(`<h1 id="数据库学习资料" tabindex="-1"><a class="header-anchor" href="#数据库学习资料" aria-hidden="true">#</a> 数据库学习资料</h1><h2 id="_1-oceanbase-官方教程" tabindex="-1"><a class="header-anchor" href="#_1-oceanbase-官方教程" aria-hidden="true">#</a> 1.oceanbase 官方教程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>https://oceanbase.github.io/miniob/miniob-introduction.html
+https://github.com/oceanbase/miniob
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-国产可持久化防redis-数据库" tabindex="-1"><a class="header-anchor" href="#_2-国产可持久化防redis-数据库" aria-hidden="true">#</a> 2.国产可持久化防redis 数据库</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>pika: https://github.com/OpenAtomFoundation/pika
+ssdb: https://github.com/ideawu/ssdb
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_3-cmu-15445-课程" tabindex="-1"><a class="header-anchor" href="#_3-cmu-15445-课程" aria-hidden="true">#</a> 3.cmu 15445 课程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>https://github.com/cmu-db/bustub
+
+官方课程:
+https://15445.courses.cs.cmu.edu/spring2024/
+
+pdf:
+https://github.com/omarhosny206/Database-System-Concepts-7th-edition
+https://www.octawian.ro/fisiere/situri/asor/build/html/_downloads/1fcab53a6d916e39c715fc20a9a9c2a8/Silberschatz_A_databases_6th_ed.pdf
+https://amirsmvt.github.io/Database/Static_files/Fundamental_of_Database_Systems.pdf
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_4-tidb官方教程" tabindex="-1"><a class="header-anchor" href="#_4-tidb官方教程" aria-hidden="true">#</a> 4.tidb官方教程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>    https://github.com/talent-plan/tinysql
+    https://github.com/talent-plan/tinykv
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_5-rust-学习资料" tabindex="-1"><a class="header-anchor" href="#_5-rust-学习资料" aria-hidden="true">#</a> 5.rust 学习资料</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>1.课程练习: https://github.com/sunface/rust-by-practice
+2.翻译文档: 
+    https://course.rs/about-book.html
+    https://kaisery.github.io/trpl-zh-cn/
+    https://blog.fudenglong.site/Programming-Rust/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_6-rust-教学数据库" tabindex="-1"><a class="header-anchor" href="#_6-rust-教学数据库" aria-hidden="true">#</a> 6.rust 教学数据库</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>    https://github.com/erikgrinaker/toydb.git
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,13),t=[n];function r(c,l){return a(),i("div",null,t)}const h=e(s,[["render",r],["__file","2024-01-15.html.vue"]]);export{h as default};
---
+++ assets/2024-01-17.html-fd480baf.js
@@ -0,0 +1,18 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as n,c as i,f as a}from"./app-3e1b09c5.js";const s={},d=a(`<h1 id="虚拟机环境搭建" tabindex="-1"><a class="header-anchor" href="#虚拟机环境搭建" aria-hidden="true">#</a> 虚拟机环境搭建</h1><h2 id="_1-wmware16-下载激活" tabindex="-1"><a class="header-anchor" href="#_1-wmware16-下载激活" aria-hidden="true">#</a> 1.wmware16 下载激活</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>下载：
+    https://www.123pan.com/s/HQeA-aX1Sh
+    https://download3.vmware.com/software/wkst/file/VMware-workstation-full-16.0.0-16894299.exe
+    
+激活码：
+    ZF3R0-FHED2-M80TY-8QYGC-NPKYF
+    YF390-0HF8P-M81RQ-2DXQE-M2UT6
+    ZF71R-DMX85-08DQY-8YMNC-PPHV8
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-ubuntu22-04-镜像下载" tabindex="-1"><a class="header-anchor" href="#_2-ubuntu22-04-镜像下载" aria-hidden="true">#</a> 2.ubuntu22.04 镜像下载</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>    iso:
+    https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/
+    https://mirrors.aliyun.com/ubuntu-releases/
+    https://cn.ubuntu.com/download/desktop
+
+    国内镜像源:
+    1.清华： https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
+    2.csdn：https://blog.csdn.net/xiangxianghehe/article/details/122856771
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),r=[d];function l(c,t){return n(),i("div",null,r)}const v=e(s,[["render",l],["__file","2024-01-17.html.vue"]]);export{v as default};
