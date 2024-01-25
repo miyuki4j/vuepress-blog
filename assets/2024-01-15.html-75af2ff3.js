e589b7b32792661de82e987301f05e0207b6ea2b
miyuki4j <39372705+miyuki4j@users.noreply.github.com>
2024-01-25 08:50:52 +0000

    Deploying to gh-pages from @ miyuki4j/vuepress-blog@c9929f1c0da2669dbd249017d801052849cc9f34 🚀

---
+++ assets/2024-01-15.html-75af2ff3.js
@@ -0,0 +1,31 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as i,f as s}from"./app-2d06e9dc.js";const d={},n=s(`<h1 id="数据库学习资料" tabindex="-1"><a class="header-anchor" href="#数据库学习资料" aria-hidden="true">#</a> 数据库学习资料</h1><h2 id="_0-awesome-databases-materials" tabindex="-1"><a class="header-anchor" href="#_0-awesome-databases-materials" aria-hidden="true">#</a> 0.awesome databases materials</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>    https://github.com/pingcap/awesome-database-learning
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="_1-oceanbase-官方教程" tabindex="-1"><a class="header-anchor" href="#_1-oceanbase-官方教程" aria-hidden="true">#</a> 1.oceanbase 官方教程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>https://oceanbase.github.io/miniob/miniob-introduction.html
+https://github.com/oceanbase/miniob
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-国产可持久化防redis-数据库" tabindex="-1"><a class="header-anchor" href="#_2-国产可持久化防redis-数据库" aria-hidden="true">#</a> 2.国产可持久化防redis 数据库</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>pika: https://github.com/OpenAtomFoundation/pika
+ssdb: https://github.com/ideawu/ssdb
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_3-cmu-15445-课程" tabindex="-1"><a class="header-anchor" href="#_3-cmu-15445-课程" aria-hidden="true">#</a> 3.cmu 15445 课程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>https://github.com/cmu-db/bustub
+
+官方课程:
+https://15445.courses.cs.cmu.edu/spring2024/
+
+pdf:
+https://github.com/omarhosny206/Database-System-Concepts-7th-edition
+https://www.octawian.ro/fisiere/situri/asor/build/html/_downloads/1fcab53a6d916e39c715fc20a9a9c2a8/Silberschatz_A_databases_6th_ed.pdf
+https://amirsmvt.github.io/Database/Static_files/Fundamental_of_Database_Systems.pdf
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_4-tidb官方教程" tabindex="-1"><a class="header-anchor" href="#_4-tidb官方教程" aria-hidden="true">#</a> 4.tidb官方教程</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>    https://github.com/talent-plan/tinysql
+    https://github.com/talent-plan/tinykv
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_5-rust-学习资料" tabindex="-1"><a class="header-anchor" href="#_5-rust-学习资料" aria-hidden="true">#</a> 5.rust 学习资料</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>1.课程练习: https://github.com/sunface/rust-by-practice
+2.翻译文档: 
+    https://course.rs/about-book.html
+    https://kaisery.github.io/trpl-zh-cn/
+    https://blog.fudenglong.site/Programming-Rust/
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_6-rust-教学数据库" tabindex="-1"><a class="header-anchor" href="#_6-rust-教学数据库" aria-hidden="true">#</a> 6.rust 教学数据库</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>    https://github.com/erikgrinaker/toydb.git
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h3 id="_7-数据库基本概念" tabindex="-1"><a class="header-anchor" href="#_7-数据库基本概念" aria-hidden="true">#</a> 7.数据库基本概念</h3><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>  B+ 树，bitcask，LSM Tree，以及 LSM Tree 的优化 Wisckey
+
+  bitcask:
+    https://riak.com/assets/bitcask-intro.pdf
+    https://github.com/rosedblabs/rosedb/blob/main/README-CN.md
+  
+  Wisckey:
+    https://www.usenix.org/system/files/conference/fast16/fast16-papers-lu.pdf
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,17),r=[n];function t(c,l){return a(),i("div",null,r)}const b=e(d,[["render",t],["__file","2024-01-15.html.vue"]]);export{b as default};
