27599c0f4b746c4824d3f8cf39d6f7d8f289e687
miyuki4j <rebirthypp@163.com>
2024-01-27 22:43:20 +0800

    add scripts.

---
+++ src/share/scripts/chrome/chrome_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+# https://linuxize.com/post/how-to-install-google-chrome-web-browser-on-ubuntu-20-04/
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
