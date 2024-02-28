d279201b0618f1cfa049cc57ccdab15c5a4cfc3f
miyuki4j <39372705+miyuki4j@users.noreply.github.com>
2024-02-28 11:08:51 +0000

    Deploying to gh-pages from @ miyuki4j/vuepress-blog@16edec0ff2c73b6ff4571404c1e35fdac837f9cc 🚀

---
+++ assets/2024-02-17.html-74ced5a8.js
@@ -0,0 +1,16 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as s,f as a}from"./app-e8311e1f.js";const n={},r=a(`<h1 id="资料收集" tabindex="-1"><a class="header-anchor" href="#资料收集" aria-hidden="true">#</a> 资料收集</h1><h2 id="_1-操作系统" tabindex="-1"><a class="header-anchor" href="#_1-操作系统" aria-hidden="true">#</a> 1.操作系统</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>
+https://github.com/Francesco601/AWESOME-Operating-System-Resources
+https://www.quora.com/What-are-the-best-resources-to-learn-an-operating-system-I-am-preparing-for-an-interview
+https://www.edx.org/learn/operating-systems
+https://github.com/himself65/learn-os
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-github-top-star-reop" tabindex="-1"><a class="header-anchor" href="#_2-github-top-star-reop" aria-hidden="true">#</a> 2.github top star reop</h2><div class="language-code line-numbers-mode" data-ext="code"><pre class="language-code"><code>按star排名:
+https://gitstar-ranking.com/repositories
+
+高星repo:
+
+https://github.com/practical-tutorials/project-based-learning
+https://github.com/sdmg15/Best-websites-a-programmer-should-visit
+https://github.com/PKUFlyingPig/cs-self-learning
+https://github.com/justjavac/free-programming-books-zh_CN
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),t=[r];function d(c,l){return i(),s("div",null,t)}const u=e(n,[["render",d],["__file","2024-02-17.html.vue"]]);export{u as default};
